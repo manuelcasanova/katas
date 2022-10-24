5b00b33b557dce99fc1ff5fe4925e9aa7eb81acc
Manuel Casanova <manucasanova@hotmail.com>
2022-10-24 11:11:32 -0700

    Kata 7

--- kata7.js
+++ kata7.js
@@ -1,4 +1,59 @@
-// Find the Missing Numbers
-// You have a bag containing tiles with numbers [1, 2, 3, …, n] written on them. Each number appears exactly once, so there are n tiles and n numbers. Now, without looking, k number tiles are randomly picked out of the bag and discarded. Create a missingNos() function that takes in a list and k, and returns the missing numbers in ascending order (from smallest to greatest).
+// Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
 
-// For example, missingNos([1, 2, 4, 5, 6, 7, 8, 10], 2) should return [3, 9].
+// For example,
+
+// [true,  true,  true,  false,
+//   true,  true,  true,  true ,
+//   true,  false, true,  false,
+//   true,  false, false, true ,
+//   true,  true,  true,  true ,
+//   false, false, true,  true]
+// The correct answer would be 17.
+
+// Hint: Don't forget to check for bad values like null/undefined
+
+let mySheeps = [true, true, true, false,
+  true, true, true, true,
+  true, false, true, false,
+  true, false, false, true,
+  true, true, true, true,
+  false, false, true, true];
+
+  //My try
+
+// function countSheeps(arrayOfSheep) {
+//   let presentSheeps = 0;
+//   for (sheep of arrayOfSheep) {
+//     if (sheep !== null || sheep !== undefined) {
+//       if (sheep === true) {
+//         presentSheeps++
+//       }
+//     }
+//   }
+//   console.log(presentSheeps)
+// }
+
+// countSheeps(mySheeps)
+
+//Alternative1
+
+function countSheeps2(arrayOfSheep) {
+  // TODO May the force be with you
+  var num = 0;
+  
+  for(var i = 0; i < arrayOfSheep.length; i++)
+    if(arrayOfSheep[i] == true)
+      num++;
+      
+  console.log(num);
+}
+
+countSheeps2(mySheeps)
+
+//Alternative2
+
+function countSheeps3(arrayOfSheeps) {
+  console.log( arrayOfSheeps.filter(Boolean).length);
+}
+
+countSheeps3(mySheeps)
